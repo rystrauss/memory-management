3bc177bab9666dccc5b943226ff94f1d89ca78ab
Ryan Strauss <rystrauss@davidson.edu>
2018-10-24 22:29:24 -0400

    Updates to frame.c and test_frame.c

--- frame.c
+++ frame.c
@@ -8,20 +8,27 @@
 #include "frame.h"
 
 #define NUM_FRAMES 1024
-#define BITMAP_LENGTH NUM_FRAMES / 64
+#define BITMAP_LENGTH (NUM_FRAMES / 64)
 #define BIT_VALUE(ll, i) ((ll << (63 - i)) >> 63)
 
 uint64_t frames_allocated;
 
 uint64_t frames_available;
 
-uint64_t bitmap[BITMAP_LENGTH];
-
 // No need to change or initialize this. This is "empty" space that simulate your physical memory.
 _Alignas(4096) char memory[MEMORY_SIZE];
 
 // You should define a bitmap to keep track of the allocated frames here.
 // Look at the handout for details on size
+uint64_t bitmap[BITMAP_LENGTH];
+
+void bit_on(uint64_t *ll, int i) {
+    *ll |= 1ULL << i;
+}
+
+void bit_off(uint64_t *ll, uint64_t i) {
+    *ll &= ~(1ULL << i & *ll);
+}
 
 void frame_init() {
     // Initialize global variables
@@ -29,7 +36,7 @@ void frame_init() {
     frames_allocated = 0;
     frames_available = NUM_FRAMES;
 
-    for (int i = 0; i < NUM_FRAMES; ++i) {
+    for (int i = 0; i < BITMAP_LENGTH; ++i) {
         bitmap[i] = 0;
     }
 }
@@ -40,9 +47,15 @@ int64_t allocate_frame(int number_frames) {
     if (!frames_available)
         return -1;
     int index = 0, start = 0;
-    while (index < NUM_FRAMES) {
-        if (index - start == number_frames)
+    while (index <= NUM_FRAMES) {
+        if (index - start == number_frames) {
+            frames_allocated += number_frames;
+            frames_available -= number_frames;
+            for (int i = start; i < index; ++i) {
+                bit_on(&bitmap[i / BITMAP_LENGTH], i % 64);
+            }
             return start;
+        }
         if (BIT_VALUE(bitmap[index / BITMAP_LENGTH], index % 64))
             start = index + 1;
         index++;
@@ -56,10 +69,14 @@ int64_t deallocate_frame(uint64_t frame_number, int number_frames) {
     if (!frames_allocated)
         return -1;
     int ret_val = number_frames;
-    for (int i = 0; i < number_frames; ++i) {
+    for (uint64_t i = frame_number; i < frame_number + number_frames; ++i) {
         if (!BIT_VALUE(bitmap[i / BITMAP_LENGTH], i % 64))
             ret_val = -1;
-        bitmap[frame_number / BITMAP_LENGTH] &= ~(1UL << frame_number & BITMAP_LENGTH);
+        else {
+            bit_off(&bitmap[frame_number / BITMAP_LENGTH], i);
+            frames_available++;
+            frames_allocated--;
+        }
     }
     return ret_val; // Return according to what's documented in the header file for this module
 }
--- test_frame.c
+++ test_frame.c
@@ -1,5 +1,10 @@
+#include "frame.h"
+#include <stdio.h>
 
 int main() {
-
+    frame_init();
+    int64_t allocate = allocate_frame(1024);
+    int64_t deallocate = deallocate_frame(0, 1024);
+    printf("%lld, %lld\n", allocate, deallocate);
     return 0;
 }
