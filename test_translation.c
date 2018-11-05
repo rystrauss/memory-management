43d2b660fe7e332139efa6dbff1d38363d8a58d4
Ryan Strauss <rystrauss@davidson.edu>
2018-11-05 16:01:05 -0500

    Update test_translation.c

--- test_translation.c
+++ test_translation.c
@@ -4,15 +4,20 @@
 
 int main() {
     frame_init();
-//    for (uint64_t i = 0; i < 256; i++) {
-//        if (!vm_map(i, i, 1, 0)) {
-//            printf("%llu\n", i);
-//        }
-//    }
+    for (uint64_t i = 0; i < 513; i++) {
+        if (!vm_map(i, i, 1, 0)) {
+            printf("%llu\n", i);
+        }
+    }
 
-    printf("%d\n", vm_map(134217728, 1, 1, 0));
-    printf("%llu\n", vm_translate(0));
-    printf("%llu\n", vm_locate(1));
+    printf("%llu\n", vm_locate(2));
+    printf("%llu\n", vm_translate(2));
+    printf("%d\n",  vm_unmap(2, 10));
+    printf("%llu\n", vm_translate(2));
+    printf("%llu\n", vm_locate(2));
+    printf("%llu\n", vm_locate(11));
+    printf("%d\n",  vm_map(2, 1, 8, 0));
+    printf("%llu\n", vm_locate(2));
 
     return 0;
 }
